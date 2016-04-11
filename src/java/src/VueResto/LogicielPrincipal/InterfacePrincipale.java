4edd1df929c30af1000c2471ceda4e9690cd0ccf
Paul Luperini <paul.luperini@phelma.grenoble-inp.fr>
2016-04-11 17:54:20 +0200

    debut interface + diary

--- src/java/src/VueResto/LogicielPrincipal/InterfacePrincipale.java
+++ src/java/src/VueResto/LogicielPrincipal/InterfacePrincipale.java
@@ -16,5 +16,62 @@ public class InterfacePrincipale{
 	public InterfaceReservation getInterfaceReservation(){
 		return this.interfaceReservation;
 	}
+
 }
 
+//import javax.swing.*;
+//import java.awt.event.*;
+//import java.awt.Dimension;
+//
+//public class InterfacePrincipale extends JFrame {
+//    public InterfacePrincipale() {
+//        super("Logiciel de Gestion du Restaurant");
+//        WindowListener l = new WindowAdapter() {
+//            public void windowClosing(WindowEvent e){
+//                System.exit(0);
+//            }
+//        };
+//        addWindowListener(l);
+//
+//        JPanel pannel = new JPanel(); // panneau d'interface principale
+//        JTabbedPane onglets = new JTabbedPane(SwingConstants.TOP);
+//
+//        
+//
+//        JButton bouton = new JButton("Réservation");
+//        JPanel pReservation = new JPanel();
+//        pReservation.add(bouton);
+//        pReservation.setPreferredSize(new Dimension(300,65));
+//        
+//        onglets.addTab("Reservation",pReservation);
+//
+//        JButton bouton2 = new JButton("SuiviCommande");
+//        JPanel pSuiviCommande = new JPanel();
+//        pSuiviCommande.add(bouton2);
+//        pSuiviCommande.setPreferredSize(new Dimension(300,65));
+//        
+//        onglets.addTab("SuiviCommande",pSuiviCommande);
+//
+//
+//        onglets.setOpaque(true);
+//
+//        pannel.add(onglets);
+//        this.setContentPane(pannel);
+//        this.setSize(320,150);
+//        this.setVisible(true);
+//
+//        JMenuBar menuBar = new JMenuBar();
+//        this.setJMenuBar(menuBar);
+//        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//        JMenu menu = new JMenu("Fichier");
+//        JMenuItem menuItem = new JMenuItem("MenuItem");
+//        menu.add(menuItem);
+//        menuBar.add(menu);
+//        this.setVisible(true);
+//
+//    }
+//    public static void main(String [] args){
+//        JFrame frame = new InterfacePrincipale();
+//
+//    }
+//}
