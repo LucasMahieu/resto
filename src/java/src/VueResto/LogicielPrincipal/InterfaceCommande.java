4f66ac9ec0732a8657f52f5239939fd0ae0e2f5a
Lucas Mahieu <lucas.mahieu@phelma.grenoble-inp.fr>
2016-04-12 14:35:31 +0200

    1ere modif de la classe inter commande

--- src/java/src/VueResto/LogicielPrincipal/InterfaceCommande.java
+++ src/java/src/VueResto/LogicielPrincipal/InterfaceCommande.java
@@ -1,11 +1,19 @@
 package VueResto.LogicielPrincipal;
 import VueResto.*;
 import ModeleResto.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class InterfaceCommande extends ObservateurCommande{
+
+	private JPanel panelCommande;
 	
 	public InterfaceCommande(){
+		this.panelCommande = new JPanel();
+	}
 	
+	public JPanel getPanel(){
+		return this.panelCommande;
 	}
 
 	public void miseAJour(){
