e5a13a627feb6a5b38c855554af1b0cf0864d9e7
FlorentinGonthier <florentin.gonthier@phelma.grenoble-inp.fr>
2016-04-12 14:11:03 +0200

    debut gestion modif

---
+++ src/sql/update.sql
@@ -0,0 +1,22 @@
+--modif plat
+Update articles
+SET prix= ' ',
+        spécialité = '',
+        quantitéArticle= '',
+where nomArticles= '',
+
+
+--modifier table
+UPDATE Tables
+SET nombrePlaceisolée= '',
+        nombrePlaceAccolée1 = ' ',
+        nombrePlaceAccolée2 = ' ',
+        nombreGroupe = ' '
+where numeroTable = ' ';
+
+--modif réservation
+Update Reservations
+SET nbPersonne= '',
+        prixTotal = ''
+where numeroReservation= ' ' ;
+
