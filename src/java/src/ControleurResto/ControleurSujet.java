32579e43e6694daca50db4a1dd9d97f4681a41c7
Paul Luperini <paul.luperini@phelma.grenoble-inp.fr>
2016-04-13 16:52:47 +0200

    Controleur

--- src/java/src/ControleurResto/ControleurSujet.java
+++ src/java/src/ControleurResto/ControleurSujet.java
@@ -11,4 +11,27 @@ public abstract class ControleurSujet {
 	public void checkerCommande(int numeroCommande){
 
 	}
+
+    public void passerReservation(String nom, String prenom, int nbPersonnes, String date, String service, String localisation){
+      //Vérification des disponibilités des tables
+      //si ok:
+      //Appel à la création de réservation dans la BD
+
+    }
+
+    public void modifierReservation(String nom, String prenom, int nbPersonnes, String date, String service, String localisation){
+      //Vérification de l'existence de la réservation 
+      //Vérification des disponibilités des tables
+      //si ok:
+      //Appel à la modification de réservation dans la BD
+
+    }
+
+    public void supprimerReservation(String nom, String prenom, int nbPersonnes, String date, String service, String localisation){
+      //Vérification de l'existence de la réservation 
+      //si ok:
+      //Appel à la suppression de réservation dans la BD
+
+    }
+
 }
