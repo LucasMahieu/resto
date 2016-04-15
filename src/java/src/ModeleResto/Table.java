f851ec0b9b83514dab97522189a84048f1a477ef
Pierre GONDOIS <pierre.gondois@phelma.grenoble-inp.fr>
2016-04-15 16:57:47 +0200

    Pierre

--- src/java/src/ModeleResto/Table.java
+++ src/java/src/ModeleResto/Table.java
@@ -2,19 +2,20 @@ package ModeleResto;
 //TO DO
 import ControleurResto;
 public class Table extends ControleurSujet{
-  public Table(){
-  }
+    public Table(){
+    }
 
 
 
-    selectionTableLibre(String localisation, int nombrePlace) {
+    consultationTableLibre(String localisation, int nombrePlace) {
 	
 	String requeteNombreMaxTable = new String("SELECT count(*) from tables group by tables.localisation");
 	//On fait la requete et on met le resultat dans un int nbTable
 	int nombreMaxTable;
 	int nbTableActuel = 0;
+	int nombrePlaceDisponible = 0;
 	
-	while (nbTableActuel <= nombreMaxTable) {
+	while ( !(nbTableActuel <= nombreMaxTable) or  ) {
 	    nbTableActuel += 1;
 	    String requete = new String("SELECT ");
 	    
@@ -26,9 +27,9 @@ public class Table extends ControleurSujet{
 	    
 	    //on calcule la somme des tables considérées
 	    requete += "SUM("
-	    for(int i = 0; i < nbTableActuel; i++) { 
-		requete += "T" + i + ".nombreplaceisolee + ";
-	    }
+		for(int i = 0; i < nbTableActuel; i++) { 
+		    requete += "T" + i + ".nombreplaceisolee + ";
+		}
 	    //on enlève le "+ " de fin
 	    requete = requete.substring(0, requete.length() - 2);
 	    requete += ")as somme";
@@ -46,39 +47,21 @@ public class Table extends ControleurSujet{
 	    for(int i = 1; i < nbTableActuel; i++) { 
 		requete += "and T" + i + ".numerotable not in SELECT numeroreservation From estreservee)";
 	    }
-	    //Il faut que les tables soient voisines s'il y en a plusieurs
-	    for(int i = 0; i < nbTableActuel; i++) { 
-		requete += "and exists (SELECT SS.numerotable" + i + ", SS.numerotable2 From sontvoisine SS where" + i + ".numerotable not in SELECT numeroreservation From estreservee)";
+	    //Il faut que les tables soient voisines s'il y en a plusieurs (on va jusqu'à -1 du coup
+	    for(int i = 0; i < nbTableActuel - 1; i++) { 
+		requete += "and exists (SELECT SS.numerotable" + i + ", SS.numerotable" + (i+1) + "From sontvoisine SS where (SS.numerotable" + i + " = T" + i + ".numerotable and SS.numerotable" + (i+1) + " = T" + (i+1) + ".numerotable) or (SS.numerotable" + i + " = T" + (i+1) + ".numerotable and SS.numerotable" + (i+1) + " = T" + (i+1) + ".numerotable)";
 	    }
-	    requete += 
+
+
+	    //on fait la requete et on met la réponse dans nombrePlaceDisponible
+	    //??? sait pas comment faire
+	    
+	    
 	}  
-
-
-	requete += "estreservee E"
-
-
-
-
+	
+	selectionTableLibre(LinkedList<Integer> liste) {
+	    
 	}
-
-
-
-
-	    --le n dépendra du nombre max de tables renvoyé par la requête d'avant
-SELECT T1.numerotable, T2.numerotable, Tn.numerotable, SUM(T1.nombreplaceisolee + T2.nombreplaceisolee + Tn.nombreplaceisolee) as somme
-
-CASE
-		when somme >= 'nombre' THEN 'OK' ELSE '0'
-END
-
-FROM estreservee E, tables T1, tables T2, tables Tn
-
-WHERE T1.numerotable not in (SELECT numeroreservation From estreservee)
-and T2.numerotable not in (SELECT numeroreservation From estreservee)
-and Tn.numerotable not in (SELECT numeroreservation From estreservee)
-and exists (SELECT SS.numerotable1, SS.numerotable2 From sontvoisine SS where (SS.numerotable1 = T1.numerotable and SS.numerotable2 = T2.numerotable) or (SS.numerotable1 = T2.numerotable and SS.numerotable2 = T2.numerotable)
-and exists (SELECT SS.numerotable1, SS.numerotable2 From sontvoisine SS where (SS.numerotable1 = T2.numerotable and SS.numerotable2 = Tn.numerotable) or (SS.numerotable1 = Tn.numerotable and SS.numerotable2 = T2.numerotable);
-
-
-
+	
+    }
 }
