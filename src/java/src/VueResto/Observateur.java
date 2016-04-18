d26cb196faec384157c4de95938fbbe9cf369467
Lucas Mahieu <lucas.mahieu@phelma.grenoble-inp.fr>
2016-04-18 11:23:51 +0200

    Mise en place propre du patron observateur

--- src/java/src/VueResto/Observateur.java
+++ src/java/src/VueResto/Observateur.java
@@ -1,11 +1,13 @@
 package VueResto;
 import ControleurResto.*;
+import java.util.*;
 
-
-public abstract class Observateur{
-	public Observateur(){
-	}
-
-	public void miseAJour(){
-	}
+public abstract class Observateur implements Observer{
+	/**
+	 * This method is called whenever the observed object is changed. 
+	 * An application calls an Observable object's notifyObservers method to have all the object's observers notified of the change.
+	 * @param o the observable object
+	 * @param arg an argument passer to the notifyObservers method
+	 */
+	public abstract void update(Observable o, Object arg);
 }
