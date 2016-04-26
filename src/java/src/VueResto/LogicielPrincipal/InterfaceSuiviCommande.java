3ada978df7de3ed718a1d6a718433c9291566f9b
Paul Luperini <paul.luperini@phelma.grenoble-inp.fr>
2016-04-26 17:08:27 +0200

    bouton Envoyer SuiviCommande

--- src/java/src/VueResto/LogicielPrincipal/InterfaceSuiviCommande.java
+++ src/java/src/VueResto/LogicielPrincipal/InterfaceSuiviCommande.java
@@ -1,4 +1,4 @@
-package VueResto.LogicielPrincipal;
+package VueResto.LogicielPrincipal;;
 import VueResto.*;
 import ModeleResto.*;
 import ControleurResto.*;
@@ -19,7 +19,9 @@ public class InterfaceSuiviCommande extends Observateur{
 	private JButton buttonRechercheSuivi;
 	private JButton buttonOuvrir;
 	private JButton buttonFermer;
+	private JButton buttonEnvoyer;
 	private SModel sModel;
+    private int numeroReservationArticle;
 	private String titre[] = {"Nom","n° Réservation","n° Table", "Nbr Personne","Etat","Temps Etat","Servi/Commandé"};
 	private JTable tableau;
 	private JScrollPane jScrollPane;
@@ -39,7 +41,7 @@ public class InterfaceSuiviCommande extends Observateur{
 	private static final int POS_Y_NOM = 30;
 	private static final int POS_X_RECHERCHE = POS_X_NOM + TAILLE_X_FIELD_NOM + 10;
 	private static final int POS_Y_RECHERCHE = 30;
-	private static final int TAILLE_X_RECHERCHE = 80;
+	private static final int TAILLE_X_RECHERCHE = 90;
 	private static final int TAILLE_Y_RECHERCHE = 20;
 	private static final int TAILLE_OUVRIR = 25;
 	private static final int POS_X_OUVRIR = POS_X_RECHERCHE + TAILLE_X_RECHERCHE  + 10;
@@ -57,6 +59,11 @@ public class InterfaceSuiviCommande extends Observateur{
 	private static final int TAILLE_Y_TAB_ART = 550;
 	private static final int POS_X_TAB_ART = POS_X_TAB + TAILLE_X_TAB + 10;
 	private static final int POS_Y_TAB_ART = POS_Y_TABLE + TAILLE_Y_FIELD_TABLE + 10;
+
+	private static final int TAILLE_X_ENVOYER = 90;
+	private static final int TAILLE_Y_ENVOYER = 20;
+	private static final int POS_X_ENVOYER = POS_X_TAB_ART+ 10;
+	private static final int POS_Y_ENVOYER = POS_Y_FERMER;
 
 	public InterfaceSuiviCommande(){
 	// PANEL PRINCIPALE
@@ -86,6 +93,11 @@ public class InterfaceSuiviCommande extends Observateur{
 		this.buttonFermer = new JButton(new ImageIcon("./ressources/supprimer_panier.png"));
 		buttonFermer.setBounds(POS_X_FERMER,POS_Y_FERMER,TAILLE_FERMER,TAILLE_FERMER);
 		panelSuiviCommande.add(buttonFermer);
+
+        //Bouton Envoyer Articles
+		this.buttonEnvoyer= new JButton("Envoyer");
+		buttonEnvoyer.setBounds(POS_X_ENVOYER,POS_Y_ENVOYER,TAILLE_X_ENVOYER,TAILLE_Y_ENVOYER);
+		panelSuiviCommande.add(buttonEnvoyer);
 
         // Tableau contenant les commandes :
         // un bouton permet d'obtenir les détails de la commande sélectionnée
@@ -125,6 +137,7 @@ public class InterfaceSuiviCommande extends Observateur{
 		buttonRechercheSuivi.addActionListener(aL);
 		buttonOuvrir.addActionListener(aL);
 		buttonFermer.addActionListener(aL);
+		buttonEnvoyer.addActionListener(aL);
 	}
 
 	public void update(Observable o, Object arg){
@@ -157,6 +170,11 @@ public class InterfaceSuiviCommande extends Observateur{
 	public JButton getButtonFermer(){
 		return this.buttonFermer;
 	}
+
+	public JButton getButtonEnvoyer(){
+		return this.buttonEnvoyer;
+	}
+
 	public JTable getTableau(){
 		return this.tableau;
 	}
@@ -180,6 +198,25 @@ public class InterfaceSuiviCommande extends Observateur{
       // Efface les données du panneau de suivi de commande précis
       System.out.println("Effet Bouton Fermer");
 
+    }
+
+    /** effet de l'appui sur le bouton Ouvrir
+     */
+    public void effetBoutonEnvoyer(){
+      System.out.println("Effet Bouton Envoyer");
+      int selectedRow = this.tableauArticles.getSelectedRow();
+      if(selectedRow >= 0){
+        System.out.println("Selected row : " + selectedRow );
+        String numArticleSelected = (String) (this.tableau.getModel()).getValueAt(selectedRow,0);
+        int quantiteEnvoyee = (int) (this.tableauArticles.getModel()).getValueAt(selectedRow,1);
+        System.out.println("Article: " + numArticleSelected);
+        System.out.println("quantite: " + quantiteEnvoyee);
+        if (numeroReservationArticle > 0){
+          Controleur.get().estEnvoye(numArticleSelected,numeroReservationArticle,quantiteEnvoyee);
+        }
+        this.miseAJourTableauArticles();
+      }
+      
     }
 
     /** effet de l'appui sur le bouton RechercheSuivi
@@ -253,15 +290,27 @@ public class InterfaceSuiviCommande extends Observateur{
       // On recharge une nouvelle table correspondant à la sélection
       int selectedRow = this.tableau.getSelectedRow();
       if(selectedRow < 0){
+        this.numeroReservationArticle = -1;
+        System.out.println("selectedRow <0");
         return;
       }
       int numeroReservationCourant = Integer.parseInt((this.tableau.getModel()).getValueAt(selectedRow,1).toString());
       if ( numeroReservationCourant <= 0){
+        System.out.println("numeroReservation <= 0");
+        this.numeroReservationArticle = -1;
         return;
       }
-      Map <String, Integer> articlesReservation = new HashMap<String, Integer>();
-      //TODO : Decommenter et tester
-      //articlesReservation = Controleur.get().aEnvoyer(numeroReservationCourant);
+      this.numeroReservationArticle = numeroReservationCourant;
+
+      System.out.println("numeroReservation :" + numeroReservationCourant);
+      Map <String, Integer> articlesReservation;
+
+      articlesReservation = Controleur.get().aEnvoyer(numeroReservationCourant);
+      
+      if( articlesReservation == null){
+        System.out.println("articlesReservation = null ");
+        return;
+      }
       System.out.println("Une liste d'article a été trouvée");
       // On affiche les articles de la réservation trouvée
       for( Map.Entry<String,Integer> articleSuivi : articlesReservation.entrySet() ){
