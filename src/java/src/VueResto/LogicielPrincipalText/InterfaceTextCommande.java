d6b97d002dfb8af3f5f272ac826048578c0e2cb9
Guillaume Anoufa <guillaume.anoufa@phelma.grenoble-inp.fr>
2016-04-26 14:32:52 +0200

    Ajout de la methode supprimerPlat

--- src/java/src/VueResto/LogicielPrincipalText/InterfaceTextCommande.java
+++ src/java/src/VueResto/LogicielPrincipalText/InterfaceTextCommande.java
@@ -1,6 +1,10 @@
 package VueResto.LogicielPrincipalText;
 import VueResto.*;
 import ControleurResto.*;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.lang.*;
 import java.util.Observable;
 import java.util.Scanner;
@@ -55,24 +59,40 @@ public class InterfaceTextCommande extends Observateur{
 	}
 	
 	public void  ajouterPlat() {
-			System.out.println("Veuillez indiquer le plat commandé");
-			/*
-			System.out.println("(1) Entrée");
-			System.out.println("(2) Plat principal");
-			System.out.println("(3) Dessert");
-			System.out.println("(4) Boisson");
-			System.out.println("(5) Menu");
-			*/
-			String plat = sc.nextLine();
-			
-			System.out.println("Veuillez indiquer la quantité de ce plat commandé");
-			int quantite = Integer.parseInt(sc.nextLine());
-			
-			
-			Controleur.get().ajouterArticle(plat, quantite, 
-				Controleur.get().getNumeroReservation(table));
+		System.out.println("Veuillez indiquer le plat commandé");
+		/*
+		System.out.println("(1) Entrée");
+		System.out.println("(2) Plat principal");
+		System.out.println("(3) Dessert");
+		System.out.println("(4) Boisson");
+		System.out.println("(5) Menu");
+		*/
+		String plat = sc.nextLine();
+		
+		System.out.println("Veuillez indiquer la quantité de ce plat commandé");
+		int quantite = Integer.parseInt(sc.nextLine());
+		
+		
+		Controleur.get().ajouterArticle(plat, quantite, 
+			Controleur.get().getNumeroReservation(table));
+	}
+	
+	public void supprimerPlat() {
+		HashMap<String, Integer> listeArticle = Controleur.get().getArticlesCommandes(Controleur.get().getNumeroReservation(table));
+		int i = 0;
+		for (Map.Entry<String, Integer> entry : listeArticle.entrySet()) {
+			String key = entry.getKey();
+   	 		int value = entry.getValue();
+			i++;
+			System.out.println("("+i+") "+ key + " quantité : "+ Integer.toString(value));
+		} 
+		System.out.println("Veuillez indiquer l'article à supprimer");
+		String aSupprimer = sc.nextLine();
+		System.out.println("Quantité à supprimer");
+		int q = Integer.parseInt(sc.nextLine());
+		Controleur.get().supprimerArticle(aSupprimer, q, Controleur.get().getNumeroReservation(table));
 	}
 	public void update(Observable o, Object arg) {
-
+		
 	}
 }
